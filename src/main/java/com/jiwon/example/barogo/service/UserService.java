7f49fecce9b45c60eebca4bcfb8382515560af7a
jiwonpark <jiwonpark@yanolja.com>
2025-03-07 23:05:38 +0900

    회원가입 시 비밀번호 BCyptPasswordEncoder 적용

--- src/main/java/com/jiwon/example/barogo/service/UserService.java
+++ src/main/java/com/jiwon/example/barogo/service/UserService.java
@@ -5,19 +5,22 @@ import com.jiwon.example.barogo.dto.UserAccountDto;
 import com.jiwon.example.barogo.entity.UserAccount;
 import com.jiwon.example.barogo.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserAccountRepository userAccountRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void createUser(UserAccountDto userAccountDto) {
         UserAccount user = new UserAccount();
 
         user.setUserName(userAccountDto.getUserName());
         user.setUserId(userAccountDto.getUserId());
-        user.setUserPassword(userAccountDto.getUserPassword());
+        //user.setUserPassword(userAccountDto.getUserPassword());
+        user.setUserPassword(passwordEncoder.encode(userAccountDto.getUserPassword()));
         userAccountRepository.save(user);
     }
 }
