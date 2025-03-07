7e48320edcf2e451117cb9cde228a325a2a47de6
jiwonpark <jiwonpark@yanolja.com>
2025-03-07 23:01:37 +0900

    spring security 라이브러리 추가 관련 예외처리

--- src/main/java/com/jiwon/example/barogo/BarogoApplication.java
+++ src/main/java/com/jiwon/example/barogo/BarogoApplication.java
@@ -2,8 +2,9 @@ package com.jiwon.example.barogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BarogoApplication {
 
     public static void main(String[] args) {
